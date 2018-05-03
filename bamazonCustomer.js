13af528f1a1496960912b00d99119422f50dd39e
Holger Mueller <9jholger@gmail.com>
2018-05-03 18:22:09 -0500

    got customer answers to console log

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,46 +16,45 @@ connection.connect(function (err) {
     if (err) throw err;
     //console.log("connect as id " + connection.threadId);
     itemsDisplay()
-    
 })
 
+//create a function to view items available for sale
 function itemsDisplay() {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log("Current inventory: ")
         console.log("ID#")
         for (let i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " " + res[i].product_name + " " + "$" + res[i].price);
+            console.log(res[i].item_id + " " + res[i].product_name + " " + "$" + res[i].price);
         }
         console.log("==========================================")
-        customerPrompt()
+        customerOrder()
     })
 }
-
-function customerPrompt() {
-    inquirer.prompt([{ 
-        name: "Bamazon",
-        message: "Please enter the ID number of the item you'd like to purchase.",
-        type: "input",
-    },
-    {
-        name: "Bamazon",
-        message: "How many of thie item would you like?",
-        type: "input"
-        /* Pass your questions in here */
-}]).then(answers => {
-            // Use user feedback for... whatever!!
-        });
-        //PUT THIS SOMEWHERE!!!: connection.end();
-        
-    }
-
-//create a function to view items available for sale
-//should show: ids, names, and prices for products
 
 //prompt user with two messages:
 //1) ask for ID of item they'd like to buy
 //2) as how many units they'd like to buy
+function customerOrder(questions) {
+
+    inquirer.prompt([{
+        name: "idInput",
+        message: "Please enter the ID number of the item you'd like to purchase.",
+        type: "input",
+    },
+    {
+        name: "quantityInput",
+        message: "How many would you like?",
+        type: "input"
+    }]).then(answers => {
+        // Use user feedback for... whatever!!
+        console.log("You chose item " + answers.idInput);
+        console.log("You'd like " + answers.quantityInput + " of them.")
+    });
+    //PUT THIS SOMEWHERE!!!: connection.end();    
+}
+
+
 
 //when customer places order, app should check if store had enough stock to fill customer's order
 //If not: console log: Insufficient quantity!, and prevent order from going through
