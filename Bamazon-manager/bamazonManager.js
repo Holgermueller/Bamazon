a1e033e45a5595027e1418ebe51154a1cf372dcf
Holger Mueller <9jholger@gmail.com>
2018-05-08 16:09:36 -0500

    redownload of node packages

---
+++ Bamazon-manager/bamazonManager.js
@@ -0,0 +1,67 @@
+//require node packages
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const colors = require('colors');
+//connect to mysql database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Kafka#678",
+    database: "bamazon_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connect as id " + connection.threadId);
+    chooseATask()
+})
+
+function chooseATask() {
+
+    let task = 
+
+    inquirer.prompt([{/* Pass your questions in here */
+        name: 'task',
+        message: 'What business shall we conduct today?'.green,
+        type: 'list',
+        choices: [
+            'View all products',
+            'View low inventory',
+            'Add to inventory',
+            'Add a new product'
+        ]
+    }]).then(answers => {
+        switch (answers.task) {
+            case "View all products":
+                viewAllProducts();
+                break;
+
+            case "View low inventory":
+                viewLowProducts();
+                break;
+
+            case "Add to inventory":
+                addToInventory();
+                break;
+
+            case "Add a new product":
+                addNewProduct();
+                break;
+
+            default:
+                break;
+        }
+    });
+}
+
+function viewAllProducts() {
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log(res);
+        console.log(err);
+    });
+
+}
+
+//connection.end()
