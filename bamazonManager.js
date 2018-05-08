ccda4f4e488b0b424c6fc49eacfbaca28b003b5a
Holger Mueller <9jholger@gmail.com>
2018-05-07 23:11:45 -0500

    began switch case

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,7 @@
 //require node packages
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const colors = require('colors');
 //connect to mysql database
 const connection = mysql.createConnection({
     host: "localhost",
@@ -13,7 +14,41 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connect as id " + connection.threadId);
-
+    chooseATask()
 })
+
+function chooseATask() {
+
+    inquirer.prompt([{/* Pass your questions in here */
+        name: 'task',
+        message: 'What business shall we conduct today?'.green,
+        type: 'list',
+        choices: [
+            'View all products',
+            'View low inventory',
+            'Add to inventory',
+            'Add a new product'
+        ]
+    }]).then(answers => {
+
+        switch (answers.task) {
+            case "view-all":
+                viewAllProducts();
+                break;
+
+            case "view-low":
+                viewLowProducts();
+                break;
+
+            case "add-inventory":
+                addToInventory();
+                break;
+
+            case "add-new":
+                addNewProduct();
+                break;
+        }
+    });
+}
 
 //connection.end()
