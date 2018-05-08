d3ba41fc73e977b8a08928f98852ee0e191598eb
Holger Mueller <9jholger@gmail.com>
2018-05-07 22:46:10 -0500

    created schema file

---
+++ schema.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM bamazon_DB.products
+LIMIT 0, 1000
+
+-- Date: 2018-05-07 22:44
+*/
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (1,'myPod','Electronics',500,98);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (2,'jeans','Mens clothing',79,10);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (3,'instant pot','home and kitchen',100,2);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (4,'television','electronics',800,5);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (5,'blu-ray player','electronics',150,5);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (6,'hair dryer','beauty',30,65);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (7,'sketch pad','crafts',25,62);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (8,'milk','grocery',2,30);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (9,'bookcase','home and kitchen',27,10);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (10,'wrist straps','fitness',18,10);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (11,'microwave','home and kitchen',30,10);
